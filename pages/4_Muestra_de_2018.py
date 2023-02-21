e6aa6cc2fecdbd35bf5c0725800ab969bafb6565
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-02-21 11:38:03 -0600

    Update 4_Muestra_de_2018.py

--- pages/4_Muestra_de_2018.py
+++ pages/4_Muestra_de_2018.py
@@ -19,7 +19,9 @@ import seaborn as sns
 import googletrans
 from googletrans import Translator
 translator = Translator()
-from streamlit.scriptrunner import get_script_run_ctx as get_report_ctx
+#from streamlit.scriptrunner import get_script_run_ctx as get_report_ctx
+
+from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
 
 #from streamlit.scrip_runner import get_script_run_ctx as get_report_ctx  
 
