e628020a45b69e924057db0cf5d82bedc7c121a8
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-02-25 20:49:03 -0600

    Update 4_Muestra_de_2018.py

--- pages/4_Muestra_de_2018.py
+++ pages/4_Muestra_de_2018.py
@@ -1434,11 +1434,11 @@ plt.subplots_adjust(hspace=0.8)
 # display the plot in Streamlit
 st.pyplot()
 
-
 import streamlit as st
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
 import altair as alt
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 
 # load BD2018 dataset
 #BD2018 = pd.read_csv('ruta/a/tu/BD2018.csv')
@@ -1452,9 +1452,15 @@ num_cols = 3
 plots_per_col = 4
 num_plots = X.shape[1] * (X.shape[1]-1) // 2
 
+# create a DataFrame for plotting
+plot_data = pd.concat([X, y], axis=1)
+plot_data = plot_data.melt(id_vars=['EsPobre'])
+
+# create a selection for highlighting points on hover
+selection = alt.selection_single(on='mouseover', nearest=True, empty='none')
+
 # iterate over all possible pairs of features
-plot_count = 0
-charts = []
+plots = []
 for i in range(X.shape[1]):
     for j in range(i + 1, X.shape[1]):
         # fit decision tree classifier
@@ -1470,32 +1476,32 @@ for i in range(X.shape[1]):
         Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
         Z = Z.reshape(xx.shape)
 
-        # create scatterplot and decision surface with Altair
-        chart = (alt.Chart(X).mark_point().encode(
-                    x=X.columns[i],
-                    y=X.columns[j],
-                    color=alt.Color('Clase:N', scale=alt.Scale(scheme='dark2'))
-                ) + alt.Chart(pd.DataFrame({'x': xx.ravel(), 'y': yy.ravel(), 'z': Z.ravel()})).mark_rect().encode(
-                    x='x:Q',
-                    y='y:Q',
-                    color=alt.Color('z:Q', scale=alt.Scale(scheme='yellowgreenblue')),
-                    opacity=alt.condition(alt.datum.z == 1, alt.value(0.5), alt.value(0))
-                )).properties(
-                    title='Decision surfaces of a decision tree',
-                    width=400,
-                    height=400
-                )
+        # plot the decision surface
+        decision_surface = alt.Chart(pd.DataFrame({'x': xx.ravel(), 'y': yy.ravel(), 'z': Z.ravel()})).mark_rect().encode(
+            x='x:Q',
+            y='y:Q',
+            color='z:O',
+            opacity=alt.value(0.4)
+        )
 
-        # add chart to list
-        charts.append(chart)
+        # plot the scatter plot
+        scatter_plot = alt.Chart(plot_data).mark_circle().encode(
+            x=X.columns[i] + ':Q',
+            y=X.columns[j] + ':Q',
+            color=alt.Color('variable:N', scale=alt.Scale(domain=['EsPobre', X.columns[i], X.columns[j]], range=['#1f77b4', '#ff7f0e', '#2ca02c'])),
+            opacity=alt.condition(selection, alt.value(0.8), alt.value(0.1)),
+            tooltip=['EsPobre'] + [alt.Tooltip(c + ':Q') for c in X.columns]
+        ).add_selection(selection)
 
-# combine all charts into a single chart using Altair's concat method
-combined_chart = alt.concat(*charts, columns=num_cols)
+        # combine the decision surface and scatter plot
+        plot = (decision_surface + scatter_plot).properties(width=250, height=250, title=f'{X.columns[i]} vs. {X.columns[j]}')
+        plots.append(plot)
 
-# display the chart in Streamlit and show data on hover using Altair's tooltip method
-st.altair_chart(combined_chart.configure_view(stroke='transparent').configure_axis(grid=False)).interactive().properties(
-    tooltip=[
-        alt.Tooltip(field, type='quantitative') for field in X.columns
-    ]
-)
+# create a chart with all the plots
+combined_chart = alt.hconcat(*plots)
+
+# display the chart in Streamlit
+st.altair_chart(combined_chart.configure_view(stroke='transparent').configure_axis(grid=False), use_container_width=True)
+
+
 
