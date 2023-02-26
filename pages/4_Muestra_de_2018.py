c4fd0bb77488b9e2fde39d691ba38c195c9e6308
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-02-26 12:24:09 -0600

    Update 4_Muestra_de_2018.py

--- pages/4_Muestra_de_2018.py
+++ pages/4_Muestra_de_2018.py
@@ -1399,7 +1399,7 @@ y = BD2018.iloc[:, -1]
 
 # define number of columns and plots per column
 num_cols = 3
-plots_per_col = 4
+plots_per_col = 6
 num_plots = X.shape[1] * (X.shape[1]-1) // 2
 
 # iterate over all possible pairs of features
