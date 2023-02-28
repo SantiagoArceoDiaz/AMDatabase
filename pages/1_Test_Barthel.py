29d2be2046e85c26cfc431eb762b203fde4df86f
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-02-28 14:22:05 -0600

    Update 1_Test_Barthel.py

--- pages/1_Test_Barthel.py
+++ pages/1_Test_Barthel.py
@@ -52,29 +52,35 @@ cotidiana. Dichas actividades son:
   10. **Continencia o incontinencia urinaria:** capacidad de contener voluntariamente los esfinteres. La escala de puntajes es: puede contener las heces (**10 puntos**), sufre episodios puntuales de incontinencia (**5 puntos**) e incontinencia habitual (**0 puntos**).
 """
 )
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
+st.write( "Video explicación sobre el Test de Barthel](https://www.youtube.com/watch?v=4E375_AzQ5I)")
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.05)
 
-progress_bar.empty()
+
+
+#st.sidebar.header("Plotting Demo")
+#st.write(
+#    """This demo illustrates a combination of plotting and animation with
+#Streamlit. We're generating a bunch of random numbers in a loop for around
+#5 seconds. Enjoy!"""
+#)
+
+#progress_bar = st.sidebar.progress(0)
+#status_text = st.sidebar.empty()
+#last_rows = np.random.randn(1, 1)
+#chart = st.line_chart(last_rows)
+
+#for i in range(1, 101):
+#    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+#    status_text.text("%i%% Complete" % i)
+#    chart.add_rows(new_rows)
+#    progress_bar.progress(i)
+#    last_rows = new_rows
+#    time.sleep(0.05)
+
+#progress_bar.empty()
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
-st.button("Re-run")
+#st.button("Re-run")
