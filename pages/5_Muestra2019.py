52060f090793028717713a4b10b3dbf1e2957449
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-03-03 08:56:15 -0600

    Update 5_Muestra2019.py

--- pages/5_Muestra2019.py
+++ pages/5_Muestra2019.py
@@ -1281,13 +1281,13 @@ with tab3:
 
     ax = sns.heatmap(cf, annot=True, cmap='Blues')
 
-    ax.set_title('Seaborn Confusion Matrix with labels\n\n');
-    ax.set_xlabel('\nPredicted Values')
-    ax.set_ylabel('Actual Values ');
+    #ax.set_title('Seaborn Confusion Matrix with labels\n\n');
+    #ax.set_xlabel('\nPredicted Values')
+    #ax.set_ylabel('Actual Values ');
 
 ## Ticket labels - List must be in alphabetical order
-    ax.xaxis.set_ticklabels(['False','True'])
-    ax.yaxis.set_ticklabels(['False','True'])
+    #ax.xaxis.set_ticklabels(['False','True'])
+    #ax.yaxis.set_ticklabels(['False','True'])
 
 ## Display the visualization of the Confusion Matrix.
     st.pyplot()
