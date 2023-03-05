f65bcaee86c0debc852007bc7c86943184867980
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-03-04 22:02:55 -0600

    Update 5_Muestra2019.py

--- pages/5_Muestra2019.py
+++ pages/5_Muestra2019.py
@@ -1603,49 +1603,6 @@ with tab4:
         conjuntos = [{0}, {1, 2, 3, 6, 7, 8, 12, 13, 14, 16, 17, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 36, 37, 39, 40, 41, 45, 46, 48, 50, 52, 53, 54, 55, 56, 57, 58, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80, 83, 84, 85, 86, 87, 88, 89, 90, 92, 95, 96, 97, 98, 100, 105, 107, 109, 112, 113, 115, 116, 117, 118, 119, 120, 121, 122, 126, 127, 128, 129, 131, 133, 134, 136, 137, 141, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 155, 156, 157, 158, 159, 161, 162, 163, 164, 165, 166, 167}, {32, 33, 4, 138, 139, 51}, {5}, {130, 9, 75, 110, 142, 18, 114, 22, 153, 123}, {10}, {11}, {15}, {35}, {38}, {81, 49, 42, 93}, {59, 43, 108}, {44, 47}, {82, 94, 106}, {91}, {99}, {101}, {102}, {103}, {104}, {111}, {160, 140, 124}, {125, 135}, {132}, {154}]
 
         # Crear un dataframe vacío
-        #df = pd.DataFrame()
-
-        # Función para actualizar el dataframe a mostrar
-        #def actualizar_df(idx):
-        #    conjunto = conjuntos[idx]
-        #    df_name = f'df_{idx}'
-        #    globals()[df_name] = dfBS.loc[conjunto]
-        #    return globals()[df_name]
-
-        # Crear un slider para seleccionar el índice del conjunto
-        #idx = st.slider("Seleccione el índice del conjunto:", 0, len(conjuntos)-1)
-
-        # Actualizar el dataframe a mostrar cuando cambie el valor del slider
-        #df = actualizar_df(idx)
-
-        # Mostrar el dataframe correspondiente al conjunto seleccionado
-        #st.write(f"Dataframe para el conjunto {conjuntos[idx]}:")
-        #st.write(df)
-	
-	# Seleccionar las columnas posteriores a la tercera
-        #cols = df.columns[3:]
-
-        # Obtener los valores promedio de cada columna
-        #valores = df[cols].mean().values.tolist()
-
-        # Agregar el primer valor al final para que coincida con el tamaño de angles
-        #valores.append(valores[0])
-
-        # Calcular los ángulos de los ejes en el gráfico de radar
-        #angles = [n / float(len(cols)) * 2 * np.pi for n in range(len(cols))]
-        #angles.append(angles[0])
-
-        # Crear el gráfico de radar
-        #fig = plt.figure(figsize=(6, 6))
-        #ax = fig.add_subplot(111, polar=True)
-        #ax.plot(angles, valores)
-        #ax.fill(angles, valores, alpha=0.3)
-
-        # Mostrar el gráfico de radar
-        #st.pyplot(fig)
-
-
-        # Crear un dataframe vacío
         df = pd.DataFrame()
 
         # Función para actualizar el dataframe a mostrar
@@ -1695,7 +1652,42 @@ with tab4:
         # Mostrar el gráfico de radar
         st.pyplot(fig)
 
-	
+        # Crear un dataframe vacío
+        df = pd.DataFrame()
+
+        # Función para actualizar el dataframe a mostrar y generar el gráfico de radar
+        def actualizar_df(idx):
+           conjunto = conjuntos[idx]
+           df_name = f'df_{idx}'
+           globals()[df_name] = dfBS.loc[conjunto]
+           df = globals()[df_name]
+           cols = df.columns[3:]
+           valores = df[cols].mean().values.tolist()
+           valores.append(valores[0])
+           angles = [n / float(len(cols)) * 2 * np.pi for n in range(len(cols))]
+           angles.append(angles[0])
+           color = df.iloc[0,-1] # Obtener el color de relleno del valor de la última columna
+           fig = plt.figure(figsize=(6, 6))
+           ax = fig.add_subplot(111, polar=True)
+           ax.plot(angles, valores, label='Promedio', color=color) # Establecer el color de relleno correspondiente
+           ax.fill(angles, valores, alpha=0.3, color=color)
+           ax.set_xticks(angles[:-1])
+           ax.set_xticklabels(cols)
+           rticks = np.arange(5, max(valores), 5)
+           ax.set_rticks(rticks)
+           # Agregar leyenda de texto con el número de filas del dataframe
+           n_rows = df.shape[0]
+           ax.text(0.5, 1.1, f'Nº de filas: {n_rows}', transform=ax.transAxes, ha='center')
+           return globals()[df_name], fig
+
+        # Crear un panel de pestañas para mostrar los dataframes y gráficos de radar correspondientes
+        tabs = st.tabs(["Conjunto "+str(i) for i in range(len(conjuntos))])
+        for i, tab in enumerate(tabs):
+           with tab:
+           df, fig = actualizar_df(i)
+           st.write(f"Dataframe para el conjunto {conjuntos[i]}:")
+           st.write(df)
+           st.pyplot(fig)	
 	
 
 
