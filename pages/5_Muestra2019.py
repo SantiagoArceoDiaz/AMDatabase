7afd4990ba0870f649dbe5f1d05b881a7bb18169
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-03-04 21:14:58 -0600

    Update 5_Muestra2019.py

--- pages/5_Muestra2019.py
+++ pages/5_Muestra2019.py
@@ -1599,39 +1599,30 @@ with tab4:
         U=dfBS.columns[1:-1]
         IND=indiscernibility(U, dfBS)
         
-
-
         # Lista de conjuntos
         conjuntos = [{0}, {1, 2, 3, 6, 7, 8, 12, 13, 14, 16, 17, 19, 20, 21, 23, 24, 25, 26, 27, 28, 29, 30, 31, 34, 36, 37, 39, 40, 41, 45, 46, 48, 50, 52, 53, 54, 55, 56, 57, 58, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80, 83, 84, 85, 86, 87, 88, 89, 90, 92, 95, 96, 97, 98, 100, 105, 107, 109, 112, 113, 115, 116, 117, 118, 119, 120, 121, 122, 126, 127, 128, 129, 131, 133, 134, 136, 137, 141, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 155, 156, 157, 158, 159, 161, 162, 163, 164, 165, 166, 167}, {32, 33, 4, 138, 139, 51}, {5}, {130, 9, 75, 110, 142, 18, 114, 22, 153, 123}, {10}, {11}, {15}, {35}, {38}, {81, 49, 42, 93}, {59, 43, 108}, {44, 47}, {82, 94, 106}, {91}, {99}, {101}, {102}, {103}, {104}, {111}, {160, 140, 124}, {125, 135}, {132}, {154}]
 
         # Crear un dataframe vacío
         df = pd.DataFrame()
 
-        # Definir el índice inicial del conjunto a mostrar
-        current_index = 0
+        # Función para actualizar el dataframe a mostrar
+        def actualizar_df(idx):
+            conjunto = conjuntos[idx]
+            df_name = f'df_{idx}'
+            globals()[df_name] = dfBS.loc[conjunto]
+        return globals()[df_name]
 
-        # Crear el slider para seleccionar el conjunto
-        selected_index = st.slider('Selecciona el conjunto:', 0, len(conjuntos)-1, current_index)
+        # Crear un slider para seleccionar el índice del conjunto
+        idx = st.slider("Seleccione el índice del conjunto:", 0, len(conjuntos)-1)
 
-        # Actualizar el índice actual
-        current_index = selected_index
+        # Actualizar el dataframe a mostrar cuando cambie el valor del slider
+        df = actualizar_df(idx)
 
-        # Obtener el conjunto correspondiente al índice actual
-        current_conjunto = conjuntos[current_index]
+        # Mostrar el dataframe correspondiente al conjunto seleccionado
+        st.write(f"Dataframe para el conjunto {conjuntos[idx]}:")
+        st.write(df)
 
 
-	# Convertir el índice del DataFrame a numérico
-        df = df.reset_index(drop=True)
-
-        # Iterar sobre la lista de conjuntos y agregar las filas correspondientes al dataframe
-        for i, conjunto in enumerate(conjuntos):
-            df_name = f'df_{i}'
-            locals()[df_name] = df.loc[conjunto]
-            df = df.append(locals()[df_name])
-
-        # Mostrar el dataframe correspondiente al conjunto
-        st.write(f"Dataframe para el conjunto {conjunto}:")
-        st.write(locals()[df_name])
 	
 	
 
