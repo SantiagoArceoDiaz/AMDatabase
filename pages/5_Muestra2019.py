e62540e554ad1034b03570d8f691210d7fb01c81
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-03-05 14:19:36 -0600

    Update 5_Muestra2019.py

--- pages/5_Muestra2019.py
+++ pages/5_Muestra2019.py
@@ -1778,8 +1778,7 @@ with tab4:
         plt.axhline(y=0.5, xmin=0, xmax=3, color='black', linewidth=2)
         plt.axvline(x=0.5, ymin=0, ymax=3, color='black', linewidth=2)
         plt.show()
-        st.pyplot()
-                                                                                                                                                                                                                                                                                                                                                                                                                                .pyplot()
+        st.pyplot()                                                                                                                                                                                                                                                                                                                                                                                                                      .pyplot()
 
 	
 	
