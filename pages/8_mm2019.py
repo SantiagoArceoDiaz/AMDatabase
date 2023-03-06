f59bf165e145f280bede90703e109084503c3cab
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-03-06 00:44:09 -0600

    Update 8_mm2019.py

--- pages/8_mm2019.py
+++ pages/8_mm2019.py
@@ -1164,10 +1164,9 @@ with tabs3:
             classes = path[-1][0][0]
             l = np.argmax(classes)
             rule += f"class: {class_names[l]} (proba: {np.round(100.0*classes[l]/np.sum(classes),2)}%)"
-        rule += f" | based on {path[-1][1]:,} samples"
-        rules += [rule]
-        
-        return rules
+            rule += f" | based on {path[-1][1]:,} samples"
+            rules += [rule]
+            return rules
 
 #class_names = BD2018['target'].unique().astype(str)#fff
 #class_names = BD2018.columns[-1].unique().astype(str)
