393927e3aa0d0ffeb8833e833a740d93a38e8427
SantiagoArceoDiaz <125905323+SantiagoArceoDiaz@users.noreply.github.com>
2023-03-06 00:43:15 -0600

    Update 8_mm2019.py

--- pages/8_mm2019.py
+++ pages/8_mm2019.py
@@ -1167,7 +1167,7 @@ with tabs3:
         rule += f" | based on {path[-1][1]:,} samples"
         rules += [rule]
         
-    return rules
+        return rules
 
 #class_names = BD2018['target'].unique().astype(str)#fff
 #class_names = BD2018.columns[-1].unique().astype(str)
